885995ec4f11770ffb7cbc46d300a22a869d0b5f
Jon Fazzaro <jon@fazzaro.com>
2025-04-25 19:41:06 -0400

    feat: bind updates through the model

--- src/App.tsx
+++ src/App.tsx
@@ -16,53 +16,43 @@ const useCenteredTree = () => {
 
 function App() {
     const [translate, containerRef] = useCenteredTree();
-    // const [leaves, setLeaves] = useState(testData())
-    // let data = toDatum(leaves[0]);
-    const data = {
-        "name": "root",
-        "attributes": {"id": "350c47f7-0084-4d4d-9c83-e62b9c68f273", "status": "doing"},
-        "children": [{
-            "name": "left",
-            "attributes": {"id": "2689489d-83a9-44cb-bc4d-9e71bf4f026f", "status": "doing"},
-            "children": [{
-                "name": "stern",
-                "attributes": {"id": "c86fb27f-9e69-4a01-b571-1e5b1cc2c456", "status": "new"},
-                "children": []
-            }]
-        }, {
-            "name": "right",
-            "attributes": {"id": "d77bf1f4-d27f-472c-b2a4-c4314f3b01e1", "status": "done"},
-            "children": [{
-                "name": "port",
-                "attributes": {"id": "655c6efb-4a9d-4a5f-bb94-5047c8d1202f", "status": "canceled"},
-                "children": []
-            }, {
-                "name": "starboard",
-                "attributes": {"id": "27a501c8-63e9-4847-93f8-6677e3cf9792", "status": "done"},
-                "children": []
-            }]
-        }]
-    }
-    const [tree, setTree] = useState(data);
+    const [leaves, setLeaves] = useState(testData())
 
-    function updateNode(id: string, update: (node: RawNodeDatum) => void) {
-        setTree(_t => {
-            const node = findNode(id, _t)
-            if (node) update(node)
-            return {..._t};
+    function updateNode(id: string, update: (leaf: Leaf) => void) {
+        setLeaves(_l => {
+            const found = findLeaf(id, _l)
+            if (found) update(found)
+            return {..._l} as Leaf;
         })
+    }
 
-        function findNode(id: string, tree: RawNodeDatum): RawNodeDatum | undefined {
-            if (tree.attributes?.id === id) return tree;
-            return tree.children?.map(c => findNode(id, c)).filter(Boolean)[0]
-        }
+    function findLeaf(id: string, root: Leaf): Leaf | undefined {
+        if (root.id === id) return root;
+        return root.children?.map(r => findLeaf(id, r)).filter(Boolean)[0]
+    }
+
+    function toDatum(root: Leaf): RawNodeDatum {
+        const result = {
+            name: root.name,
+            attributes: {
+                id: root.id,
+                status: root.status
+            },
+            children: root.children.map(toDatum)
+        };
+        console.log(result)
+        return result
+    }
+
+    function bind() {
+        setLeaves(l => ({...l}) as Leaf)
     }
 
     return (
         <>
             <h3>🪩 Disco</h3>
             <div className="tree" ref={containerRef}>
-                <Tree data={tree}
+                <Tree data={toDatum(leaves)}
                       collapsible={false}
                       zoomable={false}
                       orientation={'vertical'}
@@ -71,15 +61,24 @@ function App() {
                       pathFunc={'step'}
                       renderCustomNodeElement={renderCard}
                       onNodeClick={e => {
-                          updateNode(e.data.attributes?.id, leaf => {
-                              if (leaf?.attributes) 
-                                  leaf.attributes.status = Status.done
-                          })
+                          console.log(`clicked ${e.data.name}`)
+                          const leaf = findLeaf(id(e.data) , leaves)
+                          if (leaf) {
+                              Leaf.create({
+                                  name: `child of ${leaf.name}`,
+                                  parent: leaf
+                              })
+                              bind();
+                          }
                       }}
                 />
             </div>
         </>
     )
+    
+    function id(node: RawNodeDatum) {
+        return node.attributes?.id as string;
+    }
 
     function renderCard({nodeDatum, onNodeClick}: CustomNodeElementProps) {
         return <g className={`card ${nodeDatum.attributes?.status}`}>
@@ -103,27 +102,16 @@ function App() {
         </g>;
     }
 
-    function toDatum(root: Leaf): RawNodeDatum {
-        return {
-            name: root.name,
-            attributes: {
-                id: root.id,
-                status: root.status
-            },
-            children: root.children.map(toDatum)
-        }
-    }
-
     function testData() {
         const root = Leaf.create({name: 'root'});
         const left = Leaf.create({name: 'left', parent: root, status: Status.doing});
         const right = Leaf.create({name: 'right', parent: root});
-        const leaves: Leaf[] = [root, left, right]
-        leaves.push(Leaf.create({name: 'port', parent: right, status: Status.canceled}))
-        leaves.push(Leaf.create({name: 'starboard', parent: right, status: Status.done}))
-        leaves.push(Leaf.create({name: 'stern', parent: left}))
+        // const leaves: Leaf[] = [root, left, right]
+        // leaves.push(Leaf.create({name: 'port', parent: right, status: Status.canceled}))
+        // leaves.push(Leaf.create({name: 'starboard', parent: right, status: Status.done}))
+        // leaves.push(Leaf.create({name: 'stern', parent: left}))
 
-        return leaves;
+        return root;
     }
 
 
