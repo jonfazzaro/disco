7edd80008d8d5c6406ade0c8a277bebbdc0ae0bb
Jon Fazzaro <jon@fazzaro.com>
2025-04-26 10:43:54 -0400

    feat: multiline name change

--- src/Tree/Card.tsx
+++ src/Tree/Card.tsx
@@ -9,30 +9,33 @@ interface CardProps extends CustomNodeElementProps {
 }
 
 export function Card({nodeDatum, onNodeClick, isSelected, onChange}: CardProps) {
+    function onChangeName(event: React.FormEvent<HTMLTextAreaElement>) {
+        event.currentTarget.style.height = "auto";
+        event.currentTarget.style.height = event.currentTarget.scrollHeight + "px";
+        onChange(nodeDatum.attributes?.id as string, l => l.name = event.currentTarget.value);
+    }
+
     return <foreignObject width="102" height="102" x="-51" y="-51">
-        <div className={`card ${nodeDatum.attributes?.status}`}
+        <div className={`card ${nodeDatum.attributes?.status} ${isSelected ? 'selected' : ''}`}
              onClick={onNodeClick}>
-            {isSelected && <h3>Selected!</h3>}
-            <input className="name"
-                   name="tree-leaf-name"
-                   value={truncateString(nodeDatum.name)}
-                   onChange={onChangeName}
-                   onKeyDown={blurOnEnter}
-                   autoComplete="off"
+            <textarea className="name"
+                      name="tree-leaf-name"
+                      value={truncateString(nodeDatum.name)}
+                      onInput={onChangeName}
+                      onKeyDown={blurOnEnter}
+                      autoComplete="off"
+                      rows={1}
+                      maxLength={40}
             />{}
         </div>
     </foreignObject>
 
-    function blurOnEnter(event: React.KeyboardEvent<HTMLInputElement>) {
+    function blurOnEnter(event: React.KeyboardEvent<HTMLTextAreaElement>) {
         if (event.key === 'Enter')
             event.currentTarget.blur();
     }
 
-    function onChangeName(e: React.ChangeEvent<HTMLInputElement>) {
-        return onChange(nodeDatum.attributes?.id as string, l => l.name = e.target.value);
-    }
-
-    function truncateString(str: string, maxLength: number = 30): string {
+    function truncateString(str: string, maxLength: number = 40): string {
         return str.length > maxLength ? str.slice(0, maxLength - 3) + '...' : str;
     }
 }
