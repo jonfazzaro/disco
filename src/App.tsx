5e6a6d1a8c7538f0f7a2e56aeca05b58c5fe625d
Jon Fazzaro <jon@fazzaro.com>
2025-04-28 19:35:19 -0400

    feat: a little app chrome

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,32 @@ import './App.css'
 function App() {
     return <>
         <header>
-            <h1>🪩 Disco</h1>
+            <div className="logo">
+                <h1>🪩 Disco</h1>
+                <small>for <a href="https://www.industriallogic.com/blog/discovery-trees/">Discovery Trees</a></small>
+            </div>
+            <p>Share this URL ☝️ to collaborate!</p>
+            <button onClick={newTree}>🌳 New Tree</button>
         </header>
         <Tree forest={new FirebaseRealtimeForest(key())}/>
     </>
+}
 
-    function key() {
-        if (window.location.pathname === "/")
-            window.location.href = "/" + new RealIdGenerator().nextId()
+function key() {
+    if (noKeyInURL()) newTree();
+    return getKeyFromURL()
+}
 
-        return window.location.pathname.substring(1)
-    }
+function noKeyInURL() {
+    return window.location.pathname === "/";
+}
+
+function newTree() {
+    window.location.href = "/" + new RealIdGenerator().nextId()
+}
+
+function getKeyFromURL() {
+    return window.location.pathname.substring(1);
 }
 
 export default App
