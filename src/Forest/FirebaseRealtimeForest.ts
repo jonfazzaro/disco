71c380bb42c31b06765fbafd16d50252864c6452
Jon Fazzaro <jon@fazzaro.com>
2025-04-29 08:52:17 -0400

    clean: firebase realtime database get

--- src/Forest/FirebaseRealtimeForest.ts
+++ src/Forest/FirebaseRealtimeForest.ts
@@ -1,11 +1,14 @@
-import {get, getDatabase, onValue, ref, update} from "firebase/database";
+import {DatabaseReference, get, getDatabase, onValue, ref, update} from "firebase/database";
 import {Leaf} from "../Tree/core/leaf.ts";
 import {Forest} from "./Forest.ts";
 
 export class FirebaseRealtimeForest implements Forest {
     private database = getDatabase()
+    private _database 
     
-    constructor(private readonly key: string = "tree") { }
+    constructor(private readonly key: string = "tree") {
+        this._database = new FirebaseRealtimeDatabase(this.key)
+    }
 
     async load(callback?: (leaf: Leaf) => void) {
         if (typeof callback === 'function') 
@@ -26,14 +29,43 @@ export class FirebaseRealtimeForest implements Forest {
     }
 
     private async get() {
-        const snapshot = await get(this.getRef())
-        if (snapshot.exists())
-            return Leaf.deserialize(snapshot.val())
+        const data = await this._database.get()
+        if (data !== null)
+            return Leaf.deserialize(data.val())
 
         return Leaf.create({name: "Goal"})
     }
 
     private getRef() {
+        return ref(this.database, this.key);
+    }
+}
+
+interface RealtimeDatabase {
+    get(): Promise<any>
+    onValue(callback: (data: any) => void): void
+    update(value: object): Promise<void>
+}
+
+class FirebaseRealtimeDatabase implements RealtimeDatabase {
+    private database = getDatabase()
+    constructor(private readonly key: string) { }
+
+    async get(): Promise<any> {
+        const snapshot = await get(this.ref())
+        if (snapshot.exists())
+            return snapshot.val()
+        return Promise.resolve(null);
+    }
+
+    onValue(callback: (data: any) => void): void {
+    }
+
+    update(value: object): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+    
+    private ref(): DatabaseReference {
         return ref(this.database, this.key);
     }
 }
