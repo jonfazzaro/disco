d3d0a37b00abf79d55ff1874cef9758c34e12e61
Jon Fazzaro <jon@fazzaro.com>
2025-04-29 10:52:47 -0400

    style: page title

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
